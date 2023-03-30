b1680ecb86b3359de544f5acccb30f8b35881b2e
Paloma Wink <paloma.wink@hotmail.com>
2023-03-30 15:44:39 -0300

    Requisito 5 concluido

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,6 +5,7 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
+        {/* Essa é uma forma de atribuir um valor único a uma prop, quando ele não vem de outro lugar (linha 9) */}
         <Title headline="Planetas" />
       </div>
     );
