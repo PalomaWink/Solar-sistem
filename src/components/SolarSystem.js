b828d38036b4553d90724e0422d6667c8354b281
Paloma Wink <paloma.wink@hotmail.com>
2023-03-30 16:16:06 -0300

    Requisito 6 concluido

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,6 +9,13 @@ class SolarSystem extends React.Component {
       <div data-testid="solar-system">
         {/* Essa é uma forma de atribuir um valor único a uma prop, quando ele não vem de outro lugar (linha 9) */}
         <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
       </div>
     );
   }
